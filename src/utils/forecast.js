25d52bf660b2860314fa935b1756236adbc4c835
Enveed <sopagnarithseam.com>
2021-10-12 16:48:26 +0700

    Corrected minor error

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -3,7 +3,6 @@ const request = require("request")
 const forecast = (latitude, longtitude, callback) => {
 
     const url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + encodeURIComponent(latitude) + "&lon=" + encodeURIComponent(longtitude) + "&exclude=minutely,hourly,alerts&appid=5ac5a36de3930c0586aa7fe29d64991e&units=metric"
-    console.log(url)
     request({ url, json: true}, (error, {body}) => {
         if (error)
             callback("Unable to connect to Weather Service!", undefined)
