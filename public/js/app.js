c8330e6a46b27170c4bbc73a135a3ccd33f8fa7b
Enveed <sopagnarithseam.com>
2021-10-12 16:17:13 +0700

    Setup app for Heroku

--- public/js/app.js
+++ public/js/app.js
@@ -12,7 +12,8 @@ weatherForm.addEventListener("submit", (e) => {
 
     messageOne.textContent = "Loading..."
     messageTwo.textContent = ""
-    fetch("http://localhost:3000/weather?address=" + location).then((response) => {
+
+    fetch("/weather?address=" + location).then((response) => {
     response.json().then((weatherData) => {
         if (weatherData.error)
             return messageOne.textContent = weatherData.error
